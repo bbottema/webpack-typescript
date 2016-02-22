a3fd2c61e5d3244a4d91e767ca6931b9b2bd48ab
Benny Bottema <b.bottema@projectnibble.org>
2016-02-22 11:26:05 +0100

    Update entry.ts

--- src/entry.ts
+++ src/entry.ts
@@ -9,4 +9,5 @@ import ClassB = require('ClassB');
 var a:ClassA = new ClassA(); // direct use, this works
 
 // this compiles as it should, but in runtime the provider will not be packaged and angular will throw an error
+// the preprocessor is set up to solve this automatically
 app.run(function(myProvider: ClassB) {});
